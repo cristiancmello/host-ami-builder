4287c5f36c1c3f671624b5ee2cd6ee8aac271de9
Cristian Mello <cristianc.mello@gmail.com>
2020-03-27 10:28:04 -0300

    Add initial code

---
+++ setup-rexray-docker-plugin.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo systemctl restart rexray
+sleep 3
+
+docker plugin install rexray/ebs \
+    REXRAY_PREEMPT=true \
+    EBS_REGION=$EBS_REGION \
+    EBS_ACCESSKEY=$EBS_ACCESSKEY \
+    EBS_SECRETKEY=$EBS_SECRETKEY \
+    --grant-all-permissions
+
+docker plugin install rexray/s3fs \
+    S3FS_ACCESSKEY=$S3FS_ACCESSKEY \
+    S3FS_SECRETKEY=$S3FS_SECRETKEY \
+    --grant-all-permissions
+
+docker plugin install rexray/efs \
+    EFS_ACCESSKEY=$EFS_ACCESSKEY \
+    EFS_SECRETKEY=$EFS_SECRETKEY \
+    EFS_TAG=rexray-efs \
+    --grant-all-permissions
---
+++ setup-rexray.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+sudo curl -sSL https://rexray.io/install | sh -s -- stable
+
+sudo sh -c "cat << EOF >> /etc/rexray/config.yml
+libstorage:
+  service: ebs
+  server:
+    services:
+      ebs:
+        driver: ebs
+      s3fs:
+        driver: s3fs
+      efs:
+        driver: efs
+ebs:
+  accessKey: $EBS_ACCESSKEY
+  secretKey: $EBS_SECRETKEY
+  region: $EBS_REGION
+
+s3fs:
+  region:           $S3FS_REGION
+  accessKey:        $S3FS_ACCESSKEY
+  secretKey:        $S3FS_SECRETKEY
+  disablePathStyle: false
+
+efs:
+  accessKey:           $EFS_ACCESSKEY
+  secretKey:           $EFS_SECRETKEY
+  region:              $EFS_REGION
+  tag:                 test
+  disableSessionCache: false
+  statusMaxAttempts:   6
+  statusInitialDelay:  1s
+  statusTimeout:       2m
+EOF"
+
+# Verify env vars...
+sudo cat /etc/rexray/config.yml
+
+sudo systemctl start rexray
