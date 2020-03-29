d5c090d78f57328e14dba7ef4a8deb94a45e9ea8
Cristian Mello <cristianc.mello@gmail.com>
2020-03-29 07:41:16 -0300

    Varialibilize docker compose version

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DOCKER_COMPOSE_VERSION=1.25.4
+
 sudo yum update -y
 
 # Install Docker and set up
@@ -11,7 +13,7 @@ sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 
 # Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
